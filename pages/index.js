ad3f5cfd3bca4df7aa028e39b42b5196ba85e4fa
CelticWizard <35495540+CelticWizard@users.noreply.github.com>
2022-08-03 12:07:13 +0100

    Styling + Toast

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import Loader from '../components/Loader'
 import styles from '../styles/Home.module.css'
+import toast from 'react-hot-toast';
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <Loader></Loader>
       <Link prefetch={false} href="/chris"><a>Chris' Sales</a></Link>
+      <div>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
+    </div>
     </div>
   )
 }
